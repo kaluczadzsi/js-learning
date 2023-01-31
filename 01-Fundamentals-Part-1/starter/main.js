ceaac4ff737adb83dcaa8bdabc1b7a93678451ce
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-01-31 19:02:21 +0100

    Coding challange #3 completed

--- 01-Fundamentals-Part-1/starter/main.js
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -153,5 +153,73 @@ if (favourite !== 23) {
 
 ------------------------------------------------------------------------
 
-BOOLEAN LOGIC
+LOGICAL OPERATORS
+
+const hasDriversLicense = true;
+const hasGoodVision = true;
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+if (shouldDrive) {
+  console.log('Sarah is able to drive! 🚗');
+} else {
+  console.log('Someone else should drive...');
+}
+
+------------------------------------------------------------------------
+
+CODING CHALLANGE #3
+
+There are two gymnastics teams, Dolphins and Koalas. They compete against each
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition,
+and print it to the console. Don't forget that there can be a draw, so test for that
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a
+team only wins if it has a higher score than the other team, and the same time a
+score of at least 100 points. Hint: Use a logical operator to test for minimum
+score, as well as multiple else-if blocks
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when
+both teams have the same score and both have a score greater or equal 100
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK
+
+
+const dolphinsScore = 96 + 108 + 89;
+const koalasScore = 88 + 91 + 110;
+
+const dolphinsAVG = dolphinsScore / 3;
+const koalasAVG = koalasScore / 3;
+const minScore = 100;
+
+if (dolphinsAVG > koalasAVG) {
+  console.log('Dolphins won');
+} else if (koalasAVG > dolphinsAVG) {
+  console.log('Koalas won');
+} else {
+  console.log('DRAW');
+}
+
+if (dolphinsScore > koalasScore && dolphinsScore >= minScore) {
+  console.log('Dolphins won');
+} else if (koalasScore > dolphinsScore && koalasScore >= minScore) {
+  console.log('Koalas won');
+} else if (
+  koalasScore >= minScore &&
+  dolphinsScore >= minScore &&
+  koalasScore === dolphinsScore
+) {
+  console.log('DRAW');
+} else {
+  console.log('No team wins...');
+}
 */
