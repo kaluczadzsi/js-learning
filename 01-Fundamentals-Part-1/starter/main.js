b50ce902bab11b2e161b74338e665ca8ebcf5205
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-01-31 05:41:25 +0100

    First coding challange solved

---
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -0,0 +1,12 @@
+// Coding challange #1
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / heightMark ** 2;
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
