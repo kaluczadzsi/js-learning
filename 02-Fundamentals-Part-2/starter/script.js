8b9ce49332ed2e0fb91934c3e076c71c046433d0
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-02 08:49:08 +0100

    I learned loop in loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -317,3 +317,22 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 */
+const arr = [
+  'Uchiha',
+  'Itachi',
+  2037 - 1994,
+  'shinobi',
+  ['Yusuke', 'Kageyoshi', 'Suzuya'],
+  true,
+];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(i, arr[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------ Starting exercise ${exercise}`);
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise: ${exercise}: Lifting weight repetition ${rep} 🏋️`);
+  }
+}
