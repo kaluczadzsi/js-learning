0acbc63860f8aa7c17e47e690b21c26312b2ad43
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-02 11:05:04 +0100

    When the player wins, green background added to the body

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -23,6 +23,8 @@ document.querySelector('.check').addEventListener('click', () => {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct number!';
+    document.body.style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈Too high!';
