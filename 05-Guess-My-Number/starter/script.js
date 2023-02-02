6f7e40cec31e32b8ea43f0d3665e5407d43617c5
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-02 10:36:01 +0100

    Basic dom features learned

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+/* 
+DOM = Document Object Model (WEB API)
+*/
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 20;
+document.querySelector('.score').textContent = 60;
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
