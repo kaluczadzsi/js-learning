0bda30888da70fd200a4a3dbe54d60277e5a5018
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-03 13:44:16 +0100

    I learned how Js Engine and Runtime works

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+/* 
+JIT = Just In Time compilation
+AST = Abstract Syntax Tree
+
+code -> parsing (AST) ->
+compilation (takes AST, compiles it into machine code, (JIT)) ->
+gets executed right away (happens in callstack),
+during execution code being optimized, recompiled
+
+JS runtime in the browser:
+
+JS ENGINE + WEB APIs + CALLBACK QUEUE
+
+heart of js runtime -> JS ENGINE (HEAP, CALLSTACK)
+WEB APIs -> (DOM, FETCH API, TIMES, ETC) NOT PART OF THE JS ENGINE ITSELF
+JS get acces to APIs through the global window object
+CALLBACK QUEUE
+
+
+*/
