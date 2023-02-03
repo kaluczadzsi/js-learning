570f8c8de5e3cfff427081b891d138bda3f4d85f
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-03 16:14:24 +0100

    I learned how scoping works in JavaScript

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -85,3 +85,36 @@ BLOCK:
   var -> function scoped
 
 */
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const firstName = 'Steven';
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+
+      output = 'NEW OUTPUT';
+    }
+    // console.log(str); str is not defined
+    console.log(millenial);
+    // console.log(add(5, 2));
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1994);
+// console.log(age); cannot access
+// printAge(); cannot access
