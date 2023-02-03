e2387e2efbc380e42467f6cbc31026828c0c5030
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-03 14:04:03 +0100

    I learned how the call stack works

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -12,10 +12,37 @@ JS runtime in the browser:
 
 JS ENGINE + WEB APIs + CALLBACK QUEUE
 
-heart of js runtime -> JS ENGINE (HEAP, CALLSTACK)
+heart of js runtime -> JS ENGINE: (HEAP, CALLSTACK)
 WEB APIs -> (DOM, FETCH API, TIMES, ETC) NOT PART OF THE JS ENGINE ITSELF
 JS get acces to APIs through the global window object
 CALLBACK QUEUE
 
+------------------------------------------------------------------------
+
+EXECUTION CONTEXT
+
+Compilation 
+-> Creation of global exectuion context(for top level code) NOT INSIDE A FUNCTION!
+
+ONE EXECUTION CONTEXT per function:
+for each function call, a new execution context is created.
+
+WHAT'S INSIDE EXECUTION CONTEXT?
+
+
+Generated during 'Creation phase', right before execution -> {
+
+  1 - VARIABLE ENVIROMENT
+
+  let, const, and var declarations
+  Functions
+  arguments object
+  
+  2 - SCOPE CHAIN
+  
+  3 - THIS KEYWORD
+}
+
+ARROW FUNCTIONS: NO ARGUMENTS KEYWORD, NO THIS KEYWORD!!
 
 */
