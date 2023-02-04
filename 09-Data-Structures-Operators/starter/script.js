4ca667f45387467e1213474bef0cae5386179dc8
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-04 19:53:24 +0100

    I learned how to use destructuring arrays

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+/*
+Data needed for a later exercise
+ const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30'; */
 
 // Data needed for first part of the section
 const restaurant = {
@@ -12,6 +13,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  /* 
   openingHours: {
     thu: {
       open: 12,
@@ -25,5 +30,47 @@ const restaurant = {
       open: 0, // Open 24 hours
       close: 24,
     },
-  },
+  }, */
 };
+
+// Without destructuring
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c); // 2, 3, 4
+
+// With destructuring
+const [x, y, z] = arr;
+console.log(x, y, z); // 2, 3, 4
+
+let [main, , secondary] = restaurant.categories; // Skipping second element
+console.log(main, secondary);
+
+// Change variables without destructuring
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+// Change variables with destructuring
+[main, secondary] = [secondary, main];
+console.log(main);
+console.log(secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+
+// const [i, , j] = nested;
+// console.log(i, j);
+const [i, , [j, k]] = nested; // 2, 5, 6
+
+console.log(i, j, k);
+
+// Default values
+const [p = -1, q = -1, r = -1] = [8, 9];
+console.log(p, q, r);
