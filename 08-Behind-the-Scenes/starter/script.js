e82a44876a1d7c6f703cfe65072aab5892bb45d3
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-04 17:08:26 +0100

    Practised hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -150,4 +150,37 @@ WHY TDZ?
   accessing variables before declaration is bad practise and should be avoided;
 
   Makes const variables actually work
+
+  console.log(me);
+  console.log(job);
+  console.log(year);
+
+
+HOISTING AND TDZ IN PRACTISE
+
+  var me = 'Jonas';
+  let job = 'Teacher';
+  const year = 1991;
+
+  console.log(addDecl(2, 2));
+  console.log(addExpr(2, 2));
+  console.log(addArrow(2, 2));
+
+  function addDecl(a, b) {
+    return a + b;
+  }
+
+  const addExpr = function (a, b) {
+    return a + b;
+  };
+
+  const addArrow = (a, b) => a + b;
+
+      
+  // numProducts = undefined
+  if (!numProducts) deleteShoppingCart();
+  var numProducts = 10;
+  function deleteShoppingCart() {
+    console.log('All products deleted!');
+  }
 */
