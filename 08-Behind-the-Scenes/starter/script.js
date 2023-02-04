cc5112ec3ad5f35359c17662c346340547ca04a6
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-04 19:09:22 +0100

    I learned the differences primitives vs objects

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -311,4 +311,27 @@ THIS KEYWORD IN PRACTISE
         console.log(arguments);
         return a + b;
       };
+
+------------------------------------------------------------------------
+
+PRIMITIVES VS OBJECTS
+
+
+let age = 30;
+let oldAge = age;
+age = 31;
+console.log(age);
+console.log(oldAge);
+
+const me = {
+  name: 'Jonas',
+  age: 30,
+};
+
+const friend = me;
+friend.age = 33;
+
+console.log(friend);
+console.log(me);
+
 */
