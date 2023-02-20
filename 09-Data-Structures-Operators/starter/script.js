b66a1ec288f084f7f073a1cae8b30783d2eb58e3
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-20 06:55:30 +0100

    Strings section completed

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -944,7 +944,6 @@ name conversion working
 you're stuck. Then pause and continue!
 Afterwards, test with your own test data!
 GOOD LUCK
-*/
 
 document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
@@ -962,3 +961,19 @@ document.querySelector('button').addEventListener('click', function () {
     console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
   }
 });
+
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${type.startsWith('_Delayed') ? '⛔' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${from.slice(0, 3).toUpperCase()} ${to
+    .slice(0, 3)
+    .toUpperCase()} (${time.replace(':', 'h')})`.padStart(30);
+
+  console.log(output);
+}
+*/
