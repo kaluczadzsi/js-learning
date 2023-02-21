1c341cd4ee38313f1b6192ae5c05b17b1821cd79
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-21 19:45:10 +0100

    Adding default parameters, old way

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+// DEFAULT PARAMETERS
+const bookings = [];
+const createBooking = function (flightNum, numPassengers, price) {
+  numPassengers = numPassengers || 1;
+  price = 0;
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
