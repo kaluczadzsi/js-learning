4cb73bc209c43d963c2ecdd59ecbb951f424f7bc
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-02-21 20:15:38 +0100

    I learned how JS passing value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,11 +1,15 @@
 'use strict';
 // DEFAULT PARAMETERS
-const bookings = [];
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+/* const bookings = [];
+const createBooking = function (
+  // ES6
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
   // ES5
   // numPassengers = numPassengers || 1;
   // price = price || 199;
-
   const booking = {
     flightNum,
     numPassengers,
@@ -16,3 +20,43 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 };
 
 createBooking('LH123');
+createBooking('LH123', 2, 800);
+// Skipping parameter, leave at default
+createBooking('LH123', undefined, 5); 
+
+----------------------------------------
+
+HOW PASSING ARGUMENTS WORKS: VALUE VS REFERENCE
+
+const flight = 'LH234';
+const passenger = {
+  name: 'Uchiha Sasuke',
+  passport: 2473497758,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 2473497758) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, passenger);
+console.log(flight);
+console.log(passenger);
+
+// Two function (newPassport, checkIn) modifing the same object (passenger)
+const newPassport = person => {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(passenger);
+checkIn(flight, passenger);
+console.log(passenger);
+
+----------------------------------------
+*/
