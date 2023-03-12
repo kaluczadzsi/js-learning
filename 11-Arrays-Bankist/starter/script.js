8d5a2248eb966a9bdf83144e08ba8c04658b3aab
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-12 18:27:28 +0100

    I learned how every method works

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -271,8 +271,11 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 // EQUALITY
 console.log(movements.includes(-130));
 
-// CONDITION
+// SOME: CONDITION
 console.log(movements.some(mov => mov === -130));
 
 const anyDeposits = movements.some(mov => mov > 1500);
 console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
