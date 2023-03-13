340c05421bd98e00147ecf96bb13878765568eef
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-13 19:47:25 +0100

    I learned how Array.from works

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -322,17 +322,32 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // console.log(movements);
 // Empty arrays + fill method
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-const x = new Array(7);
-console.log(x); // empty x 7
-console.log(x.map(x => 5)); // empty x 7
-// x.fill(1);
-// console.log(x); // [1, 1, 1, 1, 1, 1, 1]
+// const x = new Array(7);
+// console.log(x); // empty x 7
+// console.log(x.map(x => 5)); // empty x 7
+// // x.fill(1);
+// // console.log(x); // [1, 1, 1, 1, 1, 1, 1]
 
-x.fill(1, 3);
-console.log(x); // [empty × 3, 1, 1, 1, 1]
+// x.fill(1, 3);
+// console.log(x); // [empty × 3, 1, 1, 1, 1]
 
-arr.fill(23, 4, 6);
-console.log(arr); // [1, 2, 3, 4, 23, 23, 7]
+// arr.fill(23, 4, 6);
+// console.log(arr); // [1, 2, 3, 4, 23, 23, 7]
+
+// Array.from
+
+const y = Array.from({ length: 7 }, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7 }, (_, i) => i + 1);
+console.log(z); // [1, 2, 3, 4, 5, 6, 7]
+
+const randomArray = Array.from(
+  { length: 100 },
+  (_, i) => i + Math.trunc(Math.random() * 100)
+);
+
+console.log(randomArray);
