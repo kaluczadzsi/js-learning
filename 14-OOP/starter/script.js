593c255c4d187259b132b80b9c9342bbd1f92ab9
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-26 13:37:09 +0200

    I learned basic of constructor functions

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,8 +1,14 @@
 'use strict';
 
 const Person = function (firstName, birthYear) {
+  // Instance properties
   this.firstName = firstName;
   this.birthYear = birthYear;
+
+  // Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
 const gabor = new Person('Gabor', 1994);
@@ -11,3 +17,10 @@ console.log(gabor);
 // 2. function is called, this = {}
 // 3. {} linked to prototype
 // 4. function automatically return {}
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(gabor instanceof Person);
+// console.log(jay instanceof Person);
