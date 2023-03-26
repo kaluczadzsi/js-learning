955cc6fb8dca9c1726413399367c15e6c41f4424
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-26 13:30:35 +0200

    First class created

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const gabor = new Person('Gabor', 1994);
+console.log(gabor);
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
