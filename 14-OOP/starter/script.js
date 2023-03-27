5e289422f0f0f38f429016e86a9bec9f5d5fcc89
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-27 20:51:50 +0200

    Coding challange 1 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -66,3 +66,28 @@ Array.prototype.unique = function () {
 console.log(array.unique());
 
 const h1 = document.querySelector('h1');
+
+// Coding challenge #1
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going with ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} is going with ${this.speed} km/h`);
+};
+
+const bmw = new Car('BMW', 120);
+const mercedes = new Car('Mercedes', 95);
+
+bmw.accelerate();
+bmw.brake();
+
+mercedes.accelerate();
