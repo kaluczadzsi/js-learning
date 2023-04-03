ae59e0c567391a68b0d387032c126f1795b4aca7
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-03 06:23:40 +0200

    I Learned how to declate private fields in class

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -420,33 +420,35 @@ jay.introduce();
 */
 
 class Account {
+  // Public fields (instances)
+  locale = navigator.language;
+  #pin;
+
+  // Private fields
+  #movements = [];
+
   constructor(owner, currency, pin) {
     this.owner = owner;
     this.currency = currency;
-    this._pin = pin;
+    this.#pin = pin;
     // Protected property
-    this._movements = [];
-    this.locale = navigator.language;
+    // this._movements = [];
+    // this.locale = navigator.language;
 
     console.log(`Thanks for opening an account, ${owner}`);
   }
 
-  // Public interface
-
+  // Public methods
   getMovements() {
-    return this._movements;
+    return this.#movements;
   }
 
   deposit(val) {
-    this._movements.push(val);
+    this.#movements.push(val);
   }
 
   withdraw(val) {
     this.deposit(-val);
-  }
-
-  _approveLoan(val) {
-    return true;
   }
 
   requestLoan(val) {
@@ -454,6 +456,11 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+  }
+
+  // Private methods
+  _approveLoan(val) {
+    return true;
   }
 }
 
@@ -463,3 +470,4 @@ acc1.deposit(250);
 acc1.withdraw(100);
 acc1.requestLoan(1000);
 console.log(acc1.getMovements());
+console.log(acc1);
