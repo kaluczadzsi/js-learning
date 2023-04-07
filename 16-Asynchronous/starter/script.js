2242a9f439968087b8aa2a4d7a64ec7e0ba5547a
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-07 20:16:01 +0200

    Coding challenge #3 completed

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -519,3 +519,58 @@ Promise.any([
 .then(data => console.log(data))
 .catch(err => console.log(err))
 */
+
+// Coding challenge #3
+/*
+
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    const img = document.createElement('img');
+    img.src = imgPath;
+    document.querySelector('.images').appendChild(img);
+    resolve(img);
+    reject(new Error('🧨'));
+  });
+};
+
+const loadNPause = async function () {
+  try {
+    // Load image 1
+    let img = await createImage('img/img-1.jpg');
+    console.log('Image 1 loaded');
+    await wait(2);
+    img.style.display = 'none';
+
+    // Load image 2
+    img = await createImage('img/img-2.jpg');
+    console.log('Image 2 loaded');
+    await wait(2);
+    img.style.display = 'none';
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+loadNPause();
+
+const loadAll = async function (...arr) {
+  try {
+    const imgs = arr.map(async img => await createImage(img));
+
+    const imgsEl = await Promise.all(imgs);
+    console.log(imgsEl);
+    imgsEl.forEach(img => img.classList.add('parallel'));
+  } catch (err) {
+    console.error(err);
+  }
+};
+loadAll('img/img-1.jpg', 'img/img-2.jpg', 'img/img-3.jpg');
+
+// // ['img/img-1.jpg','img/img-2.jpg','img/img-3.jpg']
+ */
