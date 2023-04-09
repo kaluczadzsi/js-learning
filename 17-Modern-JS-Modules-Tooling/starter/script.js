5d0b24f660930b6aeae2c149ad78799da784dcc0
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-04-09 16:58:46 +0200

    I learned how to work with import/exports

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,24 @@
+// Importing module
+// import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// ('./shoppingCart.js');
+// addToCart('bread', 5);
+// console.log(price, tq);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// console.log('Importing module');
+
+// ShoppingCart.addToCart('bread,', 5);
+// console.log(ShoppingCart.tq);
+
+// Default export
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+
+// import add from './shoppingCart.js';
+// add('pizza', 10);
+
+// import add from './shoppingCart.js';
+// import { cart } from './shoppingCart.js';
+// add('pizza', 1);
+// add('bread', 10);
+// add('apples', 4);
+// console.log(cart); [{...},{...},{...}]
